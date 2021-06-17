2918d493a9731b0ed2a9fbea65edf1782204748d
Ennui Langeweile <85590273+EnnuiL@users.noreply.github.com>
2021-06-16 23:30:26 -0300

    Fix crash in the server installer on Windows (#7)
    * Fix crash in the server installer on Windows
    Since the file names would have semicolons in it (which is an illegal character on Windows), the installer would crash. This issue is fixed by only having the library ID in the name, after all, the file's only temporary anyway
    
    * Include the artifact version

--- src/main/java/org/quiltmc/installer/action/InstallServer.java
+++ src/main/java/org/quiltmc/installer/action/InstallServer.java
@@ -255,7 +255,10 @@ public final class InstallServer extends Action<InstallServer.MessageType> {
 	private static CompletableFuture<Path> downloadLibrary(String name, String url) {
 		return CompletableFuture.supplyAsync(() -> {
 			try {
-				Path path = Files.createTempFile(name, null);
+				// Windows does not accept semicolons in filenames, so, we remove them
+				String[] splitName = name.split(":");
+				String shortName = splitName[1] + splitName[2];
+				Path path = Files.createTempFile(shortName, null);
 
 				// Convert to maven url
 				String rawUrl = mavenToUrl(url, name);
