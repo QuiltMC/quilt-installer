7b34f35835ee12967bcfbf45db9bf61efc6d412e
glitch <glitch.g3431@gmail.com>
2023-07-14 21:12:52 -0500

    Fix build

--- src/main/java/org/quiltmc/installer/gui/swing/AbstractPanel.java
+++ src/main/java/org/quiltmc/installer/gui/swing/AbstractPanel.java
@@ -18,6 +18,7 @@ package org.quiltmc.installer.gui.swing;
 
 import java.awt.*;
 import java.awt.event.ItemEvent;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.File;
