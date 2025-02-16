d37bc774e4d634c6d874b1baf85fe836a52cba69
Up <upcraftedlp@gmail.com>
2025-02-16 19:46:48 +0100

    no comment

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,6 @@ module org.quiltmc.installer {
     requires java.desktop;
     requires org.jetbrains.annotations;
     requires org.quiltmc.parsers.json;
-    requires jdk.crypto.ec;
+    requires jdk.crypto.ec; // glitch: not having this was potentially causing issues on java 8, keeping it just in case.
     requires com.google.gson;
 }
