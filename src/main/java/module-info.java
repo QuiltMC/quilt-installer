e168e04eb46a3694467cb5a31bf3a902a1008aff
Up <upcraftedlp@gmail.com>
2025-02-17 18:20:08 +0100

    update module-info file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module org.quiltmc.installer {
     requires java.desktop;
     requires org.jetbrains.annotations;
-    requires jdk.crypto.ec; // glitch: not having this was potentially causing issues on java 8, keeping it just in case.
     requires com.google.gson;
+
+    // needed so we can use x25519 when connecting to quilt meta
+    uses java.security.Provider;
 }
